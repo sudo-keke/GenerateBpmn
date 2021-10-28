9c2a4dc7355fa0ba11907ef5dfe8503b4af1fb66
yanqiang <yanqiang@360.cn>
2021-10-28 16:34:16 +0800

    first commit

---
+++ src/main/java/com/yq/activity/GenerateBpmnApplication.java
@@ -0,0 +1,13 @@
+package com.yq.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateBpmnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateBpmnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yq/activity/GenerateBpmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yq.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateBpmnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
